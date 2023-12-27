5d18730fab0074e2f9e0ce8b8cf920f6bb39fc11
umiteru2004 <umiteru2004@gmail.com>
2023-12-27 16:02:19 +0900

    feat: アニメDBのアイテム一覧を取得する関数がアイテム一覧を返すようにしました。

--- src/utils/anime_db/getAnime.js
+++ src/utils/anime_db/getAnime.js
@@ -3,7 +3,7 @@ import animeAxios from "./animeAxios";
 const getAnime = async () => {
   try {
     const response = await animeAxios.post("/query");
-    console.log(response.data.results);
+    return response.data.results;
   } catch (error) {
     console.error(error);
   }
