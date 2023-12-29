3c2f6325888cf4533e6cb028ffd8a00d496180ec
umiteru2004 <umiteru2004@gmail.com>
2023-12-29 10:36:53 +0900

    feat: レビューを取得する関数が、特定の作品のレビューのみを取得するようにしました。

--- src/utils/getReviews.js
+++ src/utils/getReviews.js
@@ -1,10 +1,17 @@
 import dbAxios from "./dbAxios";
 
-const getReviews = async () => {
+const getReviews = async (animeId) => {
   const PATH = "databases/d571c5897d854e54bd89275d12338a35/query";
 
+  const requestBody = {
+    filter: {
+      property: "anime_id",
+      rich_text: { equals: animeId },
+    },
+  };
+
   try {
-    const response = await dbAxios.post(PATH);
+    const response = await dbAxios.post(PATH, requestBody);
     return response.data.results;
   } catch (error) {
     console.error(error);
