1530a5170a5135fa3bdf721700f22ebdc8f2cf8f
umiteru2004 <umiteru2004@gmail.com>
2023-12-30 10:49:08 +0900

    feat: レビューDBのアイテムを取得する関数に、setStateを受け取る引数を追加しました。

--- src/utils/getReviews.js
+++ src/utils/getReviews.js
@@ -1,13 +1,7 @@
 import dbAxios from "./dbAxios";
 
-const getReviews = (setState, animeId) => {
+const getReviews = (setState, requestBody = {}) => {
   const PATH = "databases/d571c5897d854e54bd89275d12338a35/query";
-  const requestBody = {
-    filter: {
-      property: "anime_id",
-      rich_text: { equals: animeId },
-    },
-  };
 
   const setReviews = (response) => {
     const reviews = response.data.results;
